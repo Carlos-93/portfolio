928b5ed001a51f24dbe1fb4986d399ff6a0bdd1b
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-17 01:34:29 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+
+  return (
+    <div className="min-h-screen flex flex-col gap-10 justify-center items-center text-blue-500 text-5xl font-semibold">
+      <h1>Portfolio Carlos Araujo Galván</h1>
+      <h2>Software Developer</h2>
+    </div>
+  )
+}
