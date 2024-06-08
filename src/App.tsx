845b4a77a7b3787768ed4b8b01e420a0144f4faf
Carlos-93 <carlos93.bcn@hotmail.com>
2024-06-08 14:46:36 +0200

    chore: Add preconnect and font styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-export default function App() {
+import Sidebar from "./components/Sidebar";
+import Portfolio from "./pages/Portfolio";
 
+export default function App() {
   return (
-    <div className="min-h-screen flex flex-col gap-10 justify-center items-center text-blue-500 text-5xl font-semibold">
-      <h1>Portfolio Carlos Araujo Galván</h1>
-      <h2>Software Developer</h2>
+    <div className="min-h-screen flex bg-gray-900">
+      <Sidebar />
+      <Portfolio />
     </div>
   )
 }
---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+    return (
+        <div className="flex-1 p-16">
+            <h1 className="text-2xl font-semibold text-white">Dashboard</h1>
+            <div className="mt-4 bg-white p-4 rounded-lg shadow-lg">
+                <p className="text-gray-800">Welcome to your dashboard!</p>
+            </div>
+        </div>
+    )
+}
