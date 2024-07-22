6dd91b27c13d51b7d12d8df0b2398200a8a3c9e0
Carlos Araujo Galván <carlos93.bcn@hotmail.com>
2024-07-22 14:14:50 +0200

    chore: Update package-lock.json and package.json with react-router-dom version 6.25.1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,10 @@ export default function Home() {
             <div className="flex flex-col gap-5">
                 <h1 className="text-4xl text-gray-300">Hi, I'm <span className="font-bold">Carlos Araujo</span></h1>
                 <h2 className="text-2xl text-cyan-400">Full Stack Developer</h2>
-                <p className="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet libero at magna finibus tristique. Donec auctor, ligula eget tincidunt aliquam, mi odio ultricies nunc, eu ultricies odio odio nec nisi. Cras nec nunc auctor, ultricies purus eget, ultricies odio.</p>
+                <p className="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet libero at magna finibus tristique.
+                    Donec auctor, ligula eget tincidunt aliquam, mi odio ultricies nunc, eu ultricies odio odio nec nisi.
+                    Cras nec nunc auctor, ultricies purus eget, ultricies odio.
+                </p>
             </div>
             <div>
                 <img src="assets/profile.jpg" alt="Imagen de perfil" />
