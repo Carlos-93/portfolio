7921865299fbe3fbd7cad3d414f7056eb034d9de
Carlos-93 <carlos93.bcn@hotmail.com>
2024-07-31 00:14:41 +0200

    feat: Actualizar contenido de los componentes de la página principal

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,15 +1,14 @@
 export default function Education() {
-    
+
     return (
         <div className="flex gap-16 p-16 lg:ml-72">
             <div className="flex flex-col gap-5">
-                <h1 className="text-4xl text-gray-100">Educación</h1>
-                <h2 className="text-2xl text-gray-200 font-medium">Ingeniería en Sistemas Computacionales</h2>
-                <p className="text-gray-300">Instituto Tecnológico de Tijuana</p>
-                <p className="text-gray-300">2014 - 2019</p>
+                <h1 className="text-4xl text-gray-100">¡Hola! Soy <span className="text-cyan-400">Carlos Araujo Galván 👋</span></h1>
+                <h2 className="text-2xl text-gray-200 font-medium">Desarrollador Full Stack</h2>
+                <p className="text-gray-300">Soy un apasionado de la tecnología y la programación. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás.</p>
             </div>
             <div>
-                <img src="assets/education.jpg" alt="Imagen de mi educación" className="rounded-full w-96" />
+                <img src="assets/profile.jpg" alt="Imagen de perfil de Carlos Araujo Galván" className="rounded-full w-96" />
             </div>
         </div>
     );
