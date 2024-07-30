b748fbaa641f1cd7303c50eb1b020e8fc4ed4f15
Carlos-93 <carlos93.bcn@hotmail.com>
2024-07-31 00:47:18 +0200

    feat: Actualizar tamaño de imagen en componentes de página principal

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,13 +2,13 @@ export default function Skills() {
 
     return (
         <div className="flex gap-16 p-16 lg:ml-72">
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-5 w-full">
                 <h1 className="text-4xl text-gray-100">¡Hola! Soy <span className="text-cyan-400">Carlos Araujo Galván 👋</span></h1>
                 <h2 className="text-2xl text-gray-200 font-medium">Desarrollador Full Stack</h2>
                 <p className="text-gray-300">Soy un apasionado de la tecnología y la programación. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás.</p>
             </div>
             <div>
-                <img src="assets/profile.jpg" alt="Imagen de perfil de Carlos Araujo Galván" className="w-2/4" />
+                <img src="assets/profile.jpg" alt="Imagen de perfil de Carlos Araujo Galván" className="w-2/3 m-auto" />
             </div>
         </div>
     );
