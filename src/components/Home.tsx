13531043b3b80ed23431baa14a8365daaff6a029
Carlos-93 <carlos93.bcn@hotmail.com>
2025-01-04 00:31:42 +0100

    feat: Mejorar diseño y estructura de componentes de la página personal

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
 
     return (
-        <div className="flex gap-16 p-16 lg:ml-72">
+        <div className="flex flex-col sm:flex-row gap-16 p-11 sm:p-16 lg:ml-72">
             <div className="flex flex-col gap-5 w-full">
-                <h1 className="text-4xl text-gray-100">¡Hola! Soy <span className="text-cyan-400">Carlos Araujo Galván 👋</span></h1>
-                <h2 className="text-2xl text-gray-200 font-medium">Desarrollador Full Stack</h2>
+                <h1 className="text-gray-100 text-xl"><p>¡Bienvenido/a!</p> <p>Soy <span className="text-cyan-400 text">Carlos Araujo Galván 👋</span></p></h1>
+                <h2 className="text-lg text-gray-200 font-medium">Desarrollador Full Stack</h2>
                 <p className="text-gray-300">Soy un apasionado de la tecnología y la programación. Me encanta aprender cosas nuevas y compartir mis conocimientos con los demás.</p>
             </div>
             <div>
-                <img src="assets/profile.jpg" alt="Imagen de perfil de Carlos Araujo Galván" className="w-2/3 m-auto rounded-xl" />
+                <img src="assets/profile.jpg" alt="Imagen de perfil de Carlos Araujo Galván" className="sm:w-2/3 m-auto rounded-xl" />
             </div>
         </div>
     );
