46f24f53005096d124acd62750a2a2c769caa66b
Carlos-93 <carlos93.bcn@hotmail.com>
2025-01-10 21:21:27 +0100

    feat: Reemplazar imagen de GitHub por un SVG en constantes y añadir archivo SVG

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const socialNetworks: SocialNetwork[] = [
-    { href: "https://www.github.com/Carlos-93", src: "assets/github.png", alt: "GitHub" },
+    { href: "https://www.github.com/Carlos-93", src: "github.svg", alt: "GitHub" },
     { href: "https://www.linkedin.com/in/carlos-araujo-galvan/", src: "assets/linkedin.png", alt: "LinkedIn" },
     { href: "https://www.instagram.com/carloos_93", src: "assets/instagram.png", alt: "Instagram" },
     { href: "https://www.facebook.com/carlos.araujo.bcn/", src: "assets/facebook.png", alt: "Facebook" }
