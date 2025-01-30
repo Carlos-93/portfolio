90116bd9f557ad82e238560c694580e93994b95b
Carlos-93 <carlos93.bcn@hotmail.com>
2025-01-30 17:33:40 +0100

    feat: añadir secciones de experiencias, educación y certificados en constantes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,20 @@
+export const experiences: Experience[] = [
+    { title: "Desarrollador Web Full Stack", company: "PKF Attest", location: "Barcelona", date: "Septiembre 2024 - Actual", description: "Desarrollo de aplicaciones web y móviles", image: "profile.png" },
+    { title: "Desarrollador Web Frontend", company: "Excelsus Cic Grup SL", location: "Barcelona", date: "Octubre 2023 - Abril 2024", description: "Desarrollo de aplicaciones web y móviles", image: "profile.png" },
+];
+
+export const educations: Education[] = [
+    { title: "Técnico Superior en Desarrollo de Aplicaciones Web", company: "Monlau Centro de Estudios", location: "Barcelona", date: "Septiembre 2022 - Junio 2024", image: "profile.png" },
+    { title: "Técnico en Electromecánica de Vehículos Automóviles", company: "IES Mollet", location: "Mollet del Vallès", date: "Septiembre 2010 - Junio 2012", image: "profile.png" },
+    { title: "Graduado en Educación Secundaria Obligatoria", company: "IES Gallecs", location: "Mollet del Vallès", date: "Septiembre 2005 - Junio 2009", image: "profile.png" }
+];
+
+export const certificates: Certificate[] = [
+    { title: "Curso de Desarrollo Web Full Stack", company: "Udemy", location: "Online", date: "Abril 2024", image: "profile.png" },
+    { title: "Curso de Desarrollo Web Frontend", company: "Udemy", location: "Online", date: "Marzo 2024", image: "profile.png" },
+    { title: "Curso de Desarrollo Web Backend", company: "Udemy", location: "Online", date: "Febrero 2024", image: "profile.png" }
+];
+
 export const socialNetworks: SocialNetwork[] = [
     { href: "https://www.github.com/Carlos-93", src: "github.svg", alt: "GitHub" },
     { href: "https://www.linkedin.com/in/carlos-araujo-galvan/", src: "assets/linkedin.png", alt: "LinkedIn" },
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,28 @@
+interface Experience {
+    title: string;
+    company: string;
+    location: string;
+    date: string;
+    description: string;
+    image: string;
+}
+
+interface Education {
+    title: string;
+    company: string;
+    location: string;
+    date: string;
+    image: string;
+}
+
+interface Certificate {
+    title: string;
+    company: string;
+    location: string;
+    date: string;
+    image: string;
+}
+
 interface SocialNetwork {
     href: string;
     src: string;
