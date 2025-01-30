03850ce3acf5d5a67d37f216c5fd00e2fe11caa8
Carlos-93 <carlos93.bcn@hotmail.com>
2025-01-30 23:23:42 +0100

    feat: centrar imágenes de certificaciones en un contenedor flexible

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -9,7 +9,9 @@ export default function Certifications() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {certificates.map((certificate, index) => (
                     <div key={index} className="bg-gray-800 rounded-lg p-6 flex flex-col gap-4">
-                        <img src={certificate.image} alt={certificate.title} className="w-16 h-16 object-cover rounded-lg" />
+                        <figure className="flex justify-center">
+                            <img src={certificate.image} alt={certificate.title} />
+                        </figure>
                         <div className="flex flex-col gap-2">
                             <h3 className="text-lg font-medium text-white">{certificate.title}</h3>
                             <p className="text-gray-400">{certificate.company}</p>
