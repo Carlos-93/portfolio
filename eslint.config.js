f18a1c98ccf43f85279fcd54f09c16db16976381
Carlos Araujo <carlos93.bcn@hotmail.com>
2025-07-13 01:31:13 +0200

    Refactor: Update ESLint and TypeScript configurations for improved linting and type checking

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,23 @@
-import js from '@eslint/js';
-import typescript from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-    globalIgnores(['dist/', 'node_modules/']),
-    js.configs.recommended,
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        languageOptions: {
-            parser: typescriptParser,
-            parserOptions: {
-                ecmaVersion: 2020,
-                sourceType: 'module',
-            },
-            globals: {
-                window: 'readonly',
-                document: 'readonly',
-            },
-        },
-        plugins: {
-            '@typescript-eslint': typescript,
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-        },
-        rules: {
-            ...typescript.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-        },
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-]);
+  },
+])
