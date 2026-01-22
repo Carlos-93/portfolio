33e1063701a7a9436ae9b8f1373a17bb0ee14aed
Carlos Araujo Galván <119673306+Carlos-93@users.noreply.github.com>
2026-01-22 09:07:10 +0100

    Fix SVG attributes in DarkMode component

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -54,8 +54,8 @@ export default function DarkMode() {
 
             {/* Moon icon */}
             {isDark ? (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round" 
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-sun-high w-6 sm:w-7.5 transition-transform duration-300">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" 
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-sun-high w-6 sm:w-7.5 transition-transform duration-300">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656" />
                     <path d="M6.343 17.657l-1.414 1.414" />
                     <path d="M6.343 6.343l-1.414 -1.414" />
