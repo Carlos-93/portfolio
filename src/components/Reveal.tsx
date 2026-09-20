f9f2f3bd208516c2ba2e665804032914e5d8364c
Carlos Araujo <carlos93.bcn@hotmail.com>
2026-09-20 22:52:56 +0200

    Ajustar animaciones y estilos en componentes: modificar la clase de transición en Reveal y eliminar el componente Reveal de la sección "home" en Portfolio.

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -7,7 +7,7 @@ export default function Reveal({ children, delay = 0, className = '' }: RevealPr
 
     return (
         <div ref={ref} style={delay ? { transitionDelay: `${delay}ms` } : undefined}
-            className={`transition-[opacity,translate] duration-1500 ease-out ${inView ? 'translate-x-0 opacity-100' : 'translate-x-32 opacity-0'}${className ? ` ${className}` : ''}`}>
+            className={`transition-[opacity,translate] duration-1500 ease-out ${inView ? 'translate-x-0 opacity-100' : 'translate-x-16 opacity-0'}${className ? ` ${className}` : ''}`}>
             {children}
         </div>
     );
